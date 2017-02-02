c41f1e1aa48935717c3170ccb43461e2de16741f
David Gustafsson <david.gustafsson@ub.gu.se>
2017-02-02 13:36:38 +0100

    Bug 14957 - Rule based permission for MARC fields
    Add a rule based permission system for MARC field modifications.
    
    Test plan:
        1. Apply this patch
        2. Run updatedatabase.pl
        3. Log in to staff client
        4. Enable new syspref MARCPermissions
        5. You should see a new link "MARC field permissions" in the "Catalog"
           section under "Koha administrtion".
        6. Go to the "MARC field permissions" page
        7. Next to the page heading ("Manage MARC field permissions") there is a
           button with a question mark, click this for more information about the
           rule system.
        8. Add desired rules to the rule table, press the +-button to add.
        9. Clicking the pen-icon (edit) should allow you to edit corresponding rule.
        10. Clicking the x-icon should remove corresponding rule after confirmation.
        11. Selecting one or more rules followed by clicking the trash-icon
            should remove all selected rules after confirmation.
        12. To test your rules, import or in other way modify a record.
        13. Check that the record was modified as defined by your rules.
    
    Sponsored-by: Halland County Library
    Sponsored-by: Catalyst IT
    Sponsored-by: Gothenbug University Library

--- misc/migration_tools/bulkmarcimport.pl
+++ misc/migration_tools/bulkmarcimport.pl
@@ -124,7 +124,7 @@ if ($all) {
 #$index //= 1;
 my $using_elastic_search = (C4::Context->preference('SearchEngine') eq 'Elasticsearch');
 my $modify_biblio_marc_options = {
-    'defer_search_engine_indexing' => $using_elastic_search,
+    'defer_search_engine_indexing' => $using_elastic_search
 };
 
 my @search_engine_record_ids;
@@ -554,7 +554,16 @@ RECORD: foreach my $record (@{$marc_records}) {
                 };
                 if ($update) {
                     my $success;
-                    eval { $success = ModBiblio($record, $matched_record_id, GetFrameworkCode($matched_record_id), $modify_biblio_marc_options) };
+                    eval {
+                        $success = ModBiblio(
+                            $record,
+                            $matched_record_id,
+                            GetFrameworkCode($matched_record_id), {
+                                context => {source => 'bulkmarcimport'},
+                                'defer_search_engine_indexing' => $modify_biblio_marc_options->{'defer_search_engine_indexing'}
+                            }
+                        );
+                    };
                     if ($@) {
                         warn "ERROR: Edit biblio $matched_record_id failed: $@\n";
                         printlog( { id => $matched_record_id, op => "update", status => "ERROR" } ) if ($logfile);
