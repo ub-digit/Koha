f4ad4b47b372f7a41428525d30270e4488bb45e2
David Gustafsson <david.gustafsson@ub.gu.se>
2017-03-15 11:30:50 +0100

    Refactor bulkmarcimport and add options for plugins and templates

--- C4/Biblio.pm
+++ C4/Biblio.pm
@@ -284,7 +284,11 @@ Returns 1 on success 0 on failure
 =cut
 
 sub ModBiblio {
-    my ( $record, $biblionumber, $frameworkcode ) = @_;
+    my ( $record, $biblionumber, $frameworkcode, $options ) = @_;
+    my %mod_biblio_marc_options;
+    $mod_biblio_marc_options{'defer_search_engine_indexing'} =
+        defined $options && exists $options->{'defer_search_engine_indexing'} && $options->{'defer_search_engine_indexing'};
+
     if (!$record) {
         carp 'No record passed to ModBiblio';
         return 0;
@@ -328,7 +332,7 @@ sub ModBiblio {
     _koha_marc_update_biblioitem_cn_sort( $record, $oldbiblio, $frameworkcode );
 
     # update the MARC record (that now contains biblio and items) with the new record data
-    &ModBiblioMarc( $record, $biblionumber, $frameworkcode );
+    ModBiblioMarc( $record, $biblionumber, $frameworkcode, \%mod_biblio_marc_options );
 
     # modify the other koha tables
     _koha_modify_biblio( $dbh, $oldbiblio, $frameworkcode );
@@ -3212,7 +3216,7 @@ sub _koha_delete_biblio_metadata {
 
 =head2 ModBiblioMarc
 
-  &ModBiblioMarc($newrec,$biblionumber,$frameworkcode);
+  &ModBiblioMarc($newrec,$biblionumber,$frameworkcode,$options);
 
 Add MARC XML data for a biblio to koha
 
@@ -3223,7 +3227,10 @@ Function exported, but should NOT be used, unless you really know what you're do
 sub ModBiblioMarc {
     # pass the MARC::Record to this function, and it will create the records in
     # the marcxml field
-    my ( $record, $biblionumber, $frameworkcode ) = @_;
+    my ( $record, $biblionumber, $frameworkcode, $options ) = @_;
+    my $defer_search_engine_indexing =
+        defined $options && exists $options->{'defer_search_engine_indexing'} && $options->{'defer_search_engine_indexing'};
+
     if ( !$record ) {
         carp 'ModBiblioMarc passed an undefined record';
         return;
@@ -3285,7 +3292,7 @@ sub ModBiblioMarc {
         $m_rs->metadata( $record->as_xml_record($encoding) );
         $m_rs->store;
     }
-    ModZebra( $biblionumber, "specialUpdate", "biblioserver" );
+    ModZebra( $biblionumber, "specialUpdate", "biblioserver" ) unless $defer_search_engine_indexing;
     return $biblionumber;
 }
 
