79a95491c73b4067913ad1734092ccb267af8ba0
David Gustafsson <david.gustafsson@ub.gu.se>
2017-08-29 15:44:35 +0200

    Change hardcoded instances of lower case "or" in search queries to be compatible with ElasticSearch query syntax
    https://bugs.koha-community.org/show_bug.cgi?id=19197

--- catalogue/search.pl
+++ catalogue/search.pl
@@ -398,7 +398,7 @@ my %is_nolimit = map { $_ => 1 } @nolimits;
 if($params->{'multibranchlimit'}) {
     my $library_category = Koha::LibraryCategories->find( $params->{multibranchlimit} );
     my @libraries = $library_category->libraries;
-    my $multibranch = '('.join( " or ", map { 'branch: ' . $_->id } @libraries ) .')';
+    my $multibranch = '('.join( " OR ", map { 'branch: ' . $_->id } @libraries ) .')';
     push @limits, $multibranch if ($multibranch ne  '()');
 }
 
