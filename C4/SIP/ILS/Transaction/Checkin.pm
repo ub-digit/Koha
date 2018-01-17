0ce62a00d918092137714f8bdd831670f73805f6
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-01-17 10:55:45 +0100

    Syspref for not setting alert flag when book is available (not on loan)

--- C4/SIP/ILS/Transaction/Checkin.pm
+++ C4/SIP/ILS/Transaction/Checkin.pm
@@ -11,6 +11,7 @@ use strict;
 
 use C4::SIP::ILS::Transaction;
 
+use C4::Context;
 use C4::Circulation;
 use C4::Reserves qw( ModReserveAffect );
 use C4::Items qw( ModItemTransfer );
@@ -68,6 +69,10 @@ sub do_checkin {
     my ($return, $messages, $issue, $borrower) = AddReturn($barcode, $branch, undef, undef, $return_date);
     $self->alert(!$return);
     # ignoring messages: NotIssued, WasLost, WasTransfered
+
+    if(C4::Context->preference('SipNoAlertForAvailable') && $messages->{NotIssued}) {
+        $self->alert(0);
+    }
 
     # biblionumber, biblioitemnumber, itemnumber
     # borrowernumber, reservedate, branchcode
