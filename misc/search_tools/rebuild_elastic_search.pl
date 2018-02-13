ec2163490a98354ac41e2b4e0794a92b2d28b0db
David Gustafsson <david.gustafsson@ub.gu.se>
2018-02-13 13:47:49 +0100

    Hard code boost for title and author as band aid before providing generic solution

--- misc/search_tools/rebuild_elastic_search.pl
+++ misc/search_tools/rebuild_elastic_search.pl
@@ -160,6 +160,7 @@ sub do_reindex {
     my ( $next, $index_name ) = @_;
 
     my $indexer = Koha::SearchEngine::Elasticsearch::Indexer->new( { index => $index_name } );
+
     if ($delete) {
         # We know it's safe to not recreate the indexer because update_index
         # hasn't been called yet.
@@ -169,6 +170,10 @@ sub do_reindex {
             # to create it ourselves
             $indexer->create_index();
         }
+    }
+    elsif (C4::Context->preference('ExperimentalElasticsearchIndexing') && !$indexer->index_exists) {
+        # Create index if does not exist
+        $indexer->create_index();
     }
 
     my $count        = 0;
