6878c3a7b65ff7f93d3d7c5c96cd5ebdb60c8339
Kyle M Hall <kyle@bywatetsolutions.com>
2018-02-23 08:44:45 -0500

    Bug 20285: Lost item refund won't always pay down lost item fee first
    Due to a simple typo, the accountline passed to Koha::Account::pay from _FixAccountForLostAndReturned is not used. That means that the credit for the lost item fee may be applied to other fees before it is used on the lost fee itself.
    
    Test Plan:
    1) Find a patron with existing fines
    2) Ensure your settings will charge patrons for lost items
    3) Check out an item to a patron with existing fees that need paid
    4) Mark the item lost, charging the lost item fee
    5) Return the item
    6) Note the fee was refunded, but it paid down earlier fines first,
       and not the lost item fee first
    7) Apply this patch set
    8) Repeat steps 1 - 5
    9) Note the lost item fee is the first fee to be paid off by the lost
       item fee refund

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -2411,7 +2411,7 @@ sub _FixAccountForLostAndReturned {
             description  => "Item Returned " . $item_id,
             account_type => 'CR',
             offset_type  => 'Lost Item Return',
-            accounlines  => [$accountline],
+            lines        => [$accountline],
 
         }
     );
