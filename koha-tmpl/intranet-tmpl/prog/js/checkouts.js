3358529f7ded623ea6007b8e312449a3f5351445
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-03-19 13:14:30 +0100

    Add extended inhouse dropdown and store value in note. Allow notloan for inhouse without questions, and hide dropdown on onsite-force

--- koha-tmpl/intranet-tmpl/prog/js/checkouts.js
+++ koha-tmpl/intranet-tmpl/prog/js/checkouts.js
@@ -274,6 +274,11 @@ $(document).ready(function() {
                             onsite_checkout += " <span class='onsite_checkout'>(" + INHOUSE_USE + ")</span>";
                         }
 
+                        if ( oObj.issue_note ) {
+
+                            onsite_checkout += " <span class='onsite_checkout'>(" + oObj.issue_note + ")</span>";
+                        }
+
                         title += " "
                               + "<a href='/cgi-bin/koha/catalogue/moredetail.pl?biblionumber="
                               + oObj.biblionumber
@@ -570,6 +575,11 @@ $(document).ready(function() {
                             var onsite_checkout = '';
                             if ( oObj.onsite_checkout == 1 ) {
                                 onsite_checkout += " <span class='onsite_checkout'>(" + INHOUSE_USE + ")</span>";
+                            }
+
+                            if ( oObj.issue_note ) {
+
+                                onsite_checkout += " <span class='onsite_checkout'>(" + oObj.issue_note + ")</span>";
                             }
 
                             title += " "
