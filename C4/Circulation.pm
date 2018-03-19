2fbd255fbed470194717b41c2332cd5e26f3853f
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2018-03-19 13:14:30 +0100

    Add extended inhouse dropdown and store value in note. Allow notloan for inhouse without questions, and hide dropdown on onsite-force

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -1281,6 +1281,7 @@ sub AddIssue {
     my $onsite_checkout = $params && $params->{onsite_checkout} ? 1 : 0;
     my $switch_onsite_checkout = $params && $params->{switch_onsite_checkout};
     my $auto_renew = $params && $params->{auto_renew};
+    my $note = $params && $params->{note};
     my $dbh          = C4::Context->dbh;
     my $barcodecheck = CheckValidBarcode($barcode);
 
@@ -1379,7 +1380,8 @@ sub AddIssue {
                     date_due       => $datedue->strftime('%Y-%m-%d %H:%M:%S'),
                     branchcode     => C4::Context->userenv->{'branch'},
                     onsite_checkout => $onsite_checkout,
-                    auto_renew      => $auto_renew ? 1 : 0
+                    auto_renew      => $auto_renew ? 1 : 0,
+                    note            => $note,
                 }
               );
 
