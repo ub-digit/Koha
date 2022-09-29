a83d308f23d9c88b252af7158c98c37798f3f22d
David Gustafsson <david.gustafsson@ub.gu.se>
2022-09-29 17:18:01 +0200

    Bug 32476: Add caching for relatively expensive patron methods
    To test:
    1) Ensure tests in t/db_dependant/Koha/Patrons.t all pass
    
    Sponsored-by: Gothenburg University Library

--- C4/Circulation.pm
+++ C4/Circulation.pm
@@ -841,7 +841,7 @@ sub CanBookBeIssued {
         $issuingimpossible{DEBARRED} = 1;
     }
 
-    if ( $patron->is_expired ) {
+    if ( $patron->is_expired({ cache => 1 }) ) {
         $issuingimpossible{EXPIRED} = 1;
     }
 
@@ -927,7 +927,7 @@ sub CanBookBeIssued {
         else {
             $issuingimpossible{USERBLOCKEDWITHENDDATE} = $debarred_date;
         }
-    } elsif ( my $num_overdues = $patron->has_overdues ) {
+    } elsif ( my $num_overdues = $patron->has_overdues({ cache => 1 }) ) {
         ## patron has outstanding overdue loans
         if ( C4::Context->preference("OverduesBlockCirc") eq 'block'){
             $issuingimpossible{USERBLOCKEDOVERDUE} = $num_overdues;
@@ -1737,7 +1737,6 @@ sub AddIssue {
                 if ( $accumulate_charge > 0 ) {
                     AddIssuingCharge( $issue, $accumulate_charge, 'RENT_DAILY' );
                     $charge += $accumulate_charge;
-                    $item_unblessed->{charge} = $charge;
                 }
             }
 
@@ -2561,7 +2560,7 @@ sub MarkIssueReturned {
         my $overdue_restrictions = $patron->restrictions->search({ type => 'OVERDUES' });
         if ( C4::Context->preference('AutoRemoveOverduesRestrictions')
           && $patron->debarred
-          && !$patron->has_overdues
+          && !$patron->has_overdues({ cache => 1 })
           && $overdue_restrictions->count
         ) {
             DelUniqueDebarment({ borrowernumber => $borrowernumber, type => 'OVERDUES' });
@@ -2918,7 +2917,7 @@ sub CanBookBeRenewed {
         my $restrictionblockrenewing = C4::Context->preference('RestrictionBlockRenewing');
         $patron         = Koha::Patrons->find($borrowernumber); # FIXME Is this really useful?
         my $restricted  = $patron->is_debarred;
-        my $hasoverdues = $patron->has_overdues;
+        my $hasoverdues = $patron->has_overdues({ cache => 1 });
 
         if ( $restricted and $restrictionblockrenewing ) {
             return ( 0, 'restriction');
@@ -3199,7 +3198,7 @@ sub AddRenewal {
         my $overdue_restrictions = $patron->restrictions->search({ type => 'OVERDUES' });
         if ( $patron
           && $patron->is_debarred
-          && ! $patron->has_overdues
+          && !$patron->has_overdues({ cache => 1 })
           && $overdue_restrictions->count
         ) {
             DelUniqueDebarment({ borrowernumber => $borrowernumber, type => 'OVERDUES' });
@@ -4412,7 +4411,7 @@ sub _CanBookBeAutoRenewed {
         }
     );
 
-    if ( $patron->category->effective_BlockExpiredPatronOpacActions and $patron->is_expired ) {
+    if ( $patron->is_expired({ cache => 1 }) && $patron->category->effective_BlockExpiredPatronOpacActions ) {
         return 'auto_account_expired';
     }
 
