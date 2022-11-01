ub-digit/Koha-build

Bug 31735: Avoid re-fetcing objects from database by passing them directly instead of ids to various subroutines
To test:

1) Run the following test and make sure all pass:
  t/db_dependent/api/v1/biblios.t
  t/db_dependent/api/v1/checkouts.t
  t/db_dependent/api/v1/return_claims.t
  t/db_dependent/Circulation/CalcDateDue.t
  t/db_dependent/Circulation/CheckIfIssuedToPatron.t
  t/db_dependent/Circulation/dateexpiry.t
  t/db_dependent/Circulation/GetPendingOnSiteCheckouts.t
  t/db_dependent/Circulation/GetTopIssues.t
  t/db_dependent/Circulation_holdsqueue.t
  t/db_dependent/Circulation/IsItemIssued.t
  t/db_dependent/Circulation/issue.t
  t/db_dependent/Circulation/MarkIssueReturned.t
  t/db_dependent/Circulation/maxsuspensiondays.t
  t/db_dependent/Circulation/ReturnClaims.t
  t/db_dependent/Circulation/Returns.t
  t/db_dependent/Circulation/SwitchOnSiteCheckouts.t
  t/db_dependent/Circulation.t
  t/db_dependent/Circulation/TooMany.t
  t/db_dependent/Circulation/transferbook.t
  t/db_dependent/DecreaseLoanHighHolds.t
  t/db_dependent/Holds/DisallowHoldIfItemsAvailable.t
  t/db_dependent/HoldsQueue.t
  t/db_dependent/Holds/RevertWaitingStatus.t
  t/db_dependent/Illrequests.t
  t/db_dependent/ILSDI_Services.t
  t/db_dependent/Items.t
  t/db_dependent/Koha/Account/Line.t
  t/db_dependent/Koha/Acquisition/Order.t
  t/db_dependent/Koha/Biblio.t
  t/db_dependent/Koha/Holds.t
  t/db_dependent/Koha/Items.t
  t/db_dependent/Koha/Item.t
  t/db_dependent/Koha/Object.t
  t/db_dependent/Koha/Patrons.t
  t/db_dependent/Koha/Plugins/Circulation_hooks.t
  t/db_dependent/Koha/Pseudonymization.t
  t/db_dependent/Koha/Recalls.t
  t/db_dependent/Koha/Recall.t
  t/db_dependent/Koha/Template/Plugin/CirculationRules.t
  t/db_dependent/Letters/TemplateToolkit.t
  t/db_dependent/Members/GetAllIssues.t
  t/db_dependent/Members/IssueSlip.t
  t/db_dependent/Patron/Borrower_Discharge.t
  t/db_dependent/Patron/Borrower_PrevCheckout.t
  t/db_dependent/Reserves/GetReserveFee.t
  t/db_dependent/Re

misc/cronjobs/automatic_renewals.pl
@@ -169,7 +169,7 @@ while ( my $auto_renew = $auto_renews->next ) {
     }
 
     # CanBookBeRenewed returns 'auto_renew' when the renewal should be done by this script
-    my ( $ok, $error ) = CanBookBeRenewed( $auto_renew->borrowernumber, $auto_renew->itemnumber, undef, 1 );
+    my ( $ok, $error ) = CanBookBeRenewed( $auto_renew->borrowernumber, $auto_renew, undef, 1 );
     my $updated;
     if ( $error eq 'auto_renew' ) {
         $updated = 1;
@@ -183,16 +183,19 @@ while ( my $auto_renew = $auto_renews->next ) {
         }
         push @{ $report{ $auto_renew->borrowernumber } }, $auto_renew
             if ( $wants_messages ) && !$wants_digest;
-    } elsif ( $error eq 'too_many'
-        or $error eq 'on_reserve'
-        or $error eq 'restriction'
-        or $error eq 'overdue'
-        or $error eq 'too_unseen'
-        or $error eq 'auto_account_expired'
-        or $error eq 'auto_too_late'
-        or $error eq 'auto_too_much_oweing'
-        or $error eq 'auto_too_soon'
-        or $error eq 'item_denied_renewal' ) {
+    } elsif (
+        $error eq 'too_many' ||
+        $error eq 'on_reserve' ||
+        $error eq 'restriction' ||
+        $error eq 'overdue' ||
+        $error eq 'too_unseen' ||
+        $error eq 'auto_account_expired' ||
+        $error eq 'auto_too_late' ||
+        $error eq 'auto_too_much_oweing' ||
+        $error eq 'auto_too_soon' ||
+        $error eq 'item_denied_renewal' ||
+        $error eq 'item_issued_to_other_patron'
+    ) {
         if ( $verbose ) {
             say sprintf "Issue id: %s for borrower: %s and item: %s %s not be renewed. (%s)",
               $auto_renew->issue_id, $auto_renew->borrowernumber, $auto_renew->itemnumber, $confirm ? 'will' : 'would', $error;

ub-digit/Koha

misc/cronjobs/automatic_renewals.pl
@@ -169,7 +169,7 @@ while ( my $auto_renew = $auto_renews->next ) {
     }
 
     # CanBookBeRenewed returns 'auto_renew' when the renewal should be done by this script
-    my ( $ok, $error ) = CanBookBeRenewed( $auto_renew->borrowernumber, $auto_renew->itemnumber, undef, 1 );
+    my ( $ok, $error ) = CanBookBeRenewed( $auto_renew->patron, $auto_renew, undef, 1 );
     my $updated;
     if ( $error eq 'auto_renew' ) {
         $updated = 1;
@@ -183,16 +183,19 @@ while ( my $auto_renew = $auto_renews->next ) {
         }
         push @{ $report{ $auto_renew->borrowernumber } }, $auto_renew
             if ( $wants_messages ) && !$wants_digest;
-    } elsif ( $error eq 'too_many'
-        or $error eq 'on_reserve'
-        or $error eq 'restriction'
-        or $error eq 'overdue'
-        or $error eq 'too_unseen'
-        or $error eq 'auto_account_expired'
-        or $error eq 'auto_too_late'
-        or $error eq 'auto_too_much_oweing'
-        or $error eq 'auto_too_soon'
-        or $error eq 'item_denied_renewal' ) {
+    } elsif (
+        $error eq 'too_many' ||
+        $error eq 'on_reserve' ||
+        $error eq 'restriction' ||
+        $error eq 'overdue' ||
+        $error eq 'too_unseen' ||
+        $error eq 'auto_account_expired' ||
+        $error eq 'auto_too_late' ||
+        $error eq 'auto_too_much_oweing' ||
+        $error eq 'auto_too_soon' ||
+        $error eq 'item_denied_renewal' ||
+        $error eq 'item_issued_to_other_patron'
+    ) {
         if ( $verbose ) {
             say sprintf "Issue id: %s for borrower: %s and item: %s %s not be renewed. (%s)",
               $auto_renew->issue_id, $auto_renew->borrowernumber, $auto_renew->itemnumber, $confirm ? 'will' : 'would', $error;