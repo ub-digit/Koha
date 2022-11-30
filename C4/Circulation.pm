ub-digit/Koha-build

Bug 31735: Remove Koha::Config::SysPref->find since bypasses cache
get_yaml_pref_hash also allows invalid YAML and only parses a limited
subset so remove this method to avoid future issues.

C4/Circulation.pm
@@ -2090,7 +2090,7 @@ sub AddReturn {
     my $borrowernumber = $patron ? $patron->borrowernumber : undef;    # we don't know if we had a borrower or not
     my $patron_unblessed = $patron ? $patron->unblessed : {};
 
-    my $update_loc_rules = Koha::Config::SysPrefs->find('UpdateItemLocationOnCheckin')->get_yaml_pref_hash();
+    my $update_loc_rules = C4::Context::yaml_preference->('UpdateItemLocationOnCheckin');
     map { $update_loc_rules->{$_} = $update_loc_rules->{$_}[0] } keys %$update_loc_rules; #We can only move to one location so we flatten the arrays
     if ($update_loc_rules) {
         if (defined $update_loc_rules->{_ALL_}) {
@@ -4421,7 +4421,7 @@ sub _item_denied_renewal {
     my $item = $params->{item};
     return unless $item;
 
-    my $denyingrules = Koha::Config::SysPrefs->find('ItemsDeniedRenewal')->get_yaml_pref_hash();
+    my $denyingrules = C4::Context->yaml_preference('ItemsDeniedRenewal');
     return unless $denyingrules;
     foreach my $field (keys %$denyingrules) {
         my $val = $item->$field;

ub-digit/Koha

C4/Circulation.pm
@@ -2098,7 +2098,7 @@ sub AddReturn {
     my $borrowernumber = $patron ? $patron->borrowernumber : undef;    # we don't know if we had a borrower or not
     my $patron_unblessed = $patron ? $patron->unblessed : {};
 
-    my $update_loc_rules = Koha::Config::SysPrefs->find('UpdateItemLocationOnCheckin')->get_yaml_pref_hash();
+    my $update_loc_rules = C4::Context->yaml_preference('UpdateItemLocationOnCheckin');
     map { $update_loc_rules->{$_} = $update_loc_rules->{$_}[0] } keys %$update_loc_rules; #We can only move to one location so we flatten the arrays
     if ($update_loc_rules) {
         if (defined $update_loc_rules->{_ALL_}) {