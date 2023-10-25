ub-digit/Koha-build

Bug 29440: Fix AddBiblio/ModBiblio options args

misc/migration_tools/bulkmarcimport.pl
@@ -131,8 +131,12 @@ if ($all) {
 my $using_elastic_search = (C4::Context->preference('SearchEngine') eq 'Elasticsearch');
 my $mod_biblio_options = {
     disable_autolink => $using_elastic_search,
-    defer_search_engine_indexing => $using_elastic_search,
+    skip_record_index => $using_elastic_search,
     overlay_context => { source => 'bulkmarcimport' }
+};
+my $add_biblio_options = {
+    disable_autolink => $using_elastic_search,
+    skip_record_index => $using_elastic_search
 };
 
 my @search_engine_record_ids;
@@ -574,7 +578,7 @@ RECORD: foreach my $record (@{$marc_records}) {
             elsif ($insert) {
                 my $record_clone = $record->clone();
                 C4::Biblio::_strip_item_fields($record_clone);
-                eval { ($record_id, $biblioitemnumber) = AddBiblio($record_clone, $framework, { disable_autolink => 1 }) };
+                eval { ($record_id, $biblioitemnumber) = AddBiblio($record, $framework, $add_biblio_options) };
                 if ($@) {
                     warn "ERROR: Insert biblio $originalid failed: $@\n";
                     printlog( { id => $originalid, op => "insert", status => "ERROR" } ) if ($logfile);

ub-digit/Koha

misc/migration_tools/bulkmarcimport.pl
@@ -131,8 +131,12 @@ if ($all) {
 my $using_elastic_search = (C4::Context->preference('SearchEngine') eq 'Elasticsearch');
 my $mod_biblio_options = {
     disable_autolink => $using_elastic_search,
-    defer_search_engine_indexing => $using_elastic_search,
+    skip_record_index => $using_elastic_search,
     overlay_context => { source => 'bulkmarcimport' }
+};
+my $add_biblio_options = {
+    disable_autolink => $using_elastic_search,
+    skip_record_index => $using_elastic_search
 };
 
 my @search_engine_record_ids;
@@ -574,7 +578,7 @@ RECORD: foreach my $record (@{$marc_records}) {
             elsif ($insert) {
                 my $record_clone = $record->clone();
                 C4::Biblio::_strip_item_fields($record_clone);
-                eval { ($record_id, $biblioitemnumber) = AddBiblio($record_clone, $framework, { disable_autolink => 1 }) };
+                eval { ($record_id, $biblioitemnumber) = AddBiblio($record_clone, $framework, $add_biblio_options) };
                 if ($@) {
                     warn "ERROR: Insert biblio $originalid failed: $@\n";
                     printlog( { id => $originalid, op => "insert", status => "ERROR" } ) if ($logfile);